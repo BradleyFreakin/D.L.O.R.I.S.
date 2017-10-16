d764b9971acdcb1f6a374fa7c263207fc456a4d8
Clay <31814548+CashewClay@users.noreply.github.com>
2017-10-15 19:19:18 -0700

    Update 10/15

--- Site/home.php
+++ Site/home.php
@@ -10,49 +10,50 @@
 <script src="javascript/functions.js" type="text/javascript"></script>
 </head>
 <body>
-	<div class="landing">
-		<div class="layer-bg">
-			<div class="l_bgRepeat"></div>
-			<div class="l_graydient"></div>
 
-			<?php include "assets/branch.svg"; ?>
-			<?php include "assets/evolve.svg"; ?>
-			<?php include "assets/think.svg"; ?>
-		</div>
+<div class="landing">
+	<div class="layer-bg">
+		<div class="l_bgRepeat"></div>
+		<div class="l_graydient"></div>
 
-		<div id="l_links-top" class="links-social flex-distribute hidden">
-			<a class="nounderline" href="#">Contact</a>
-			<div>
-				<a class="nounderline" href="#">
-					<?php include "assets/social_icons/yt_icon.svg"; ?>
-				</a>
-				<a class="nounderline" href="#">
-					<?php include "assets/social_icons/tw_icon.svg"; ?>
-				</a>
-				<a class="nounderline" href="#">
-					<?php include "assets/social_icons/fb_icon.svg"; ?>
-				</a>
-				<a class="nounderline" href="#">
-					<?php include "assets/social_icons/insta_icon.svg"; ?>
-				</a>
-			</div>
-		</div>
+		<?php include "assets/branch.svg"; ?>
+		<?php include "assets/evolve.svg"; ?>
+		<?php include "assets/think.svg"; ?>
+	</div>
 
-		<h1 class="hero-title hidden">Paragon</h1>
-		<h2 class="hero-desc hidden"><i>noun.</i> an idea or thing regarded as a model of excellence</h2>
-
-		<div id="l_links-bottom" class="float-bottom flex-space hidden">
-			<a class="l_link" href="#we">Who We Are</a>
-			<a class="l_link" href="#about">What We Do</a>
-			<a class="l_link" href="#projects">Our Projects</a>
+	<div id="l_links-top" class="links-social flex-distribute hidden">
+		<a class="nounderline" href="#">Contact</a>
+		<div>
+			<a class="nounderline" href="#">
+				<?php include "assets/social_icons/yt_icon.svg"; ?>
+			</a>
+			<a class="nounderline" href="#">
+				<?php include "assets/social_icons/tw_icon.svg"; ?>
+			</a>
+			<a class="nounderline" href="#">
+				<?php include "assets/social_icons/fb_icon.svg"; ?>
+			</a>
+			<a class="nounderline" href="#">
+				<?php include "assets/social_icons/insta_icon.svg"; ?>
+			</a>
 		</div>
 	</div>
+
+	<h1 class="hero-title hidden">Paragon</h1>
+	<h2 class="hero-desc hidden"><i>n.</i> an idea or thing regarded as a model of excellence</h2>
+
+	<div id="l_links-bottom" class="float-bottom flex-space hidden">
+		<a class="l_link" href="#we">Who We Are</a>
+		<a class="l_link" href="#about">What We Do</a>
+		<a class="l_link" href="#projects">Our Projects</a>
+	</div>
+</div>
 
 <main>
 	<div class="main">
 		<h2 id="we">Who We Are</h2>
 		<p>
-			<img src="#" height="250px" width="400px" style="float:right;border:1px solid white;" alt="dummy pic">
+			<img class="img-float-right dummyimg" src="#" height="300px" width="400px" alt="dummy pic">
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper eu neque et hendrerit. Fusce condimentum, urna id vestibulum luctus, sem magna lobortis tellus, id pellentesque dolor nisl non odio. Curabitur ac faucibus massa, sed facilisis urna. Aliquam varius diam lacus, non maximus libero feugiat nec. In ornare libero leo, et pellentesque nulla laoreet et. Donec a convallis nisl. In felis metus, interdum id tellus viverra, cursus cursus odio. Sed ut aliquet magna, vitae egestas purus. Suspendisse blandit quis elit at suscipit. Proin in nibh magna. Morbi pretium urna quis leo semper vulputate. Etiam viverra dui in quam semper, eu tempus lacus commodo. Donec ornare iaculis nisi, ac ornare metus tempor posuere. Nullam at finibus risus, nec finibus lectus. Aliquam et sagittis magna. Pellentesque scelerisque sed nisi nec vehicula.
 		</p>
 		<p>
@@ -72,7 +73,7 @@
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper eu neque et hendrerit. Fusce condimentum, urna id vestibulum luctus, sem magna lobortis tellus, id pellentesque dolor nisl non odio. Curabitur ac faucibus massa, sed facilisis urna. Aliquam varius diam lacus, non maximus libero feugiat nec. In ornare libero leo, et pellentesque nulla laoreet et. Donec a convallis nisl. In felis metus, interdum id tellus viverra, cursus cursus odio. Sed ut aliquet magna, vitae egestas purus. Suspendisse blandit quis elit at suscipit. Proin in nibh magna. Morbi pretium urna quis leo semper vulputate. Etiam viverra dui in quam semper, eu tempus lacus commodo. Donec ornare iaculis nisi, ac ornare metus tempor posuere. Nullam at finibus risus, nec finibus lectus. Aliquam et sagittis magna. Pellentesque scelerisque sed nisi nec vehicula.
 		</p>
 		<p>
-			<img src="#" height="250px" width="300px" style="float:left;border:1px solid white;" alt="dummy pic">
+			<img class="img-float-left dummyimg" src="#" height="300px" width="300px" alt="dummy pic">
 			Sed sodales molestie elit, quis vehicula justo lacinia at. Praesent nec laoreet libero. Sed venenatis vel metus eget dignissim. Cras pulvinar eget felis et tristique. Suspendisse at sapien lacinia, mollis purus vitae, egestas ipsum. Suspendisse potenti. In blandit volutpat sem, quis vulputate nisi rutrum eu. Quisque bibendum dui rutrum eros pretium tincidunt. Phasellus in risus gravida, malesuada sem sed, tempor risus. Ut risus eros, pretium vitae egestas vel, mattis vitae magna. In luctus sem in leo dignissim fermentum.
 		</p>
 		<p>
@@ -93,9 +94,8 @@
 
 
 <!--
-Naming Scheme
-
-[fullname]
-	[f_child]
-		[f_c_grandchild]
+Naming Scheme:
+(fullname)
+	(f_child)
+		(f_c_grandchild)
 -->
---
+++ Site/styles.js
@@ -0,0 +1,16 @@
+"Colors" {
+    "purple": "#cc00ff",
+    "electric blue": "#00ccff",
+    "bright purple": "#ff00ff",
+    "rich blue": "#0099ff",
+    "gray": "#222222"
+}
+"Fonts" {
+    "serif": "Comorant Garamond",
+    "big header": "Aldrich",
+    "small header": "Wire One",
+    "ui and other": "Electrolize"
+}
+"Transitions" {
+    "links": "200ms"
+}
